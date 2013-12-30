52ff640081afbdc6dc9fdd87fbbc464560299a4c
Per Liedman <per@liedman.net>
2013-12-30 13:49:46 +0100

    Added table of contents

--- build.js
+++ build.js
@@ -2,11 +2,30 @@
 
 var _ = require('underscore'),
     marked = require('marked'),
-    fs = require('fs');
+    fs = require('fs'),
+    renderer = new marked.Renderer(),
+    toc = [];
 
-var content = marked(fs.readFileSync('README.md', 'utf8'), {smartypants: true});
+renderer.heading = function(text, level) {
+    var escapedText = text.toLowerCase().replace(/[^\w]+/g, '-');
+
+    if (level <= 2) {
+        console.log(escapedText + ": " + text);
+        toc.push({text: text, name: escapedText});
+    }
+
+    return '<h' + level + '><a name="' +
+                    escapedText +
+                     '" class="anchor" href="#' +
+                     escapedText +
+                     '"><span class="header-link"></span></a>' +
+                      text + '</h' + level + '>';
+};
+
+var content = marked(fs.readFileSync('README.md', 'utf8'), {smartypants: true, renderer: renderer});
 
 fs.writeFileSync('index.html',
     _.template(fs.readFileSync('template._', 'utf8'))({
+        toc: toc,
         content: content
     }));
